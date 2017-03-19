ce5c660261eba755768229aff8e7644ff41fb3b2
Matt Buland <gik0geck0@gmail.com>
2017-03-19 14:52:28 -0600

    Better error recover

--- aur_install.sh
+++ aur_install.sh
@@ -15,10 +15,16 @@
 #### etc packages ####
 # curl
 # sudo
+if [ -z "$1" ]; then
+    echo "Usage: $0 <package>"
+    echo "eg: $0 bauerbill"
+    exit
+fi
 
 # We're inside a location where we can run "makepkg -s" to try and create the AUR package
 function makepkg_with_dependency_resolve() {
-    build_errors=$(yes | makepkg -s --noconfirm 2>&1)
+    build_errors=$(yes | makepkg -s --skippgpcheck --noconfirm 2>&1)
+    build_status=$?
     missing_targets=$(echo "$build_errors" | grep 'error: target not found:')
     if [ ! -z "$missing_targets" ]; then
         first_dep=$(echo "$missing_targets" | head -n1 | perl -n -e'/error: target not found: ([a-zA-Z0-9-]+)/ && print $1')
@@ -31,12 +37,20 @@ function makepkg_with_dependency_resolve() {
         # This is done "just in case" an installed dep installs another missing dep
         makepkg_with_dependency_resolve
 
+    elif [ ! "$build_status" = "0" ]; then
+        echo "Build errors in makepkg:"
+        echo "$build_errors"
+        exit
     # else # assume makepkg finished successfully =D
     fi
 }
 
 function install_package() {
     tar_url=$(curl -s "https://aur.archlinux.org/packages/$1" | perl -n -e'/<a href="([^ "]+)">Download snapshot<\/a>/ && print $1')
+    if [ -z "$tar_url" ]; then
+        echo "Error, could not find download URL for package $1"
+        exit
+    fi
     curl -s "https://aur.archlinux.org/$tar_url" > package.tar.gz
     tar -xaf package.tar.gz
     pushd $1
